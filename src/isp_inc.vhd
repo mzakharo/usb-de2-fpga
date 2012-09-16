120eb7fff5e290adcc341f2b02d0afde0c0cff12
mzakharo <mzakharo@gmail.com>
2012-09-16 11:53:50 -0400

    added copyright notice

--- src/isp_inc.vhd
+++ src/isp_inc.vhd
@@ -1,18 +1,21 @@
 -- isp_inc.vhd
 -- -----------------------------------------------------------------------
--- This file is part of "ISP1362 Inteface for DE2"
+--  Copyright © 2012 Mikhail Zakharov 
+-- -----------------------------------------------------------------------
 --
---   "ISP1362 Inteface for DE2" is free software: you can redistribute it and/or modify
+-- This file is part of "ISP1362 VHDL interface for DE2"
+--
+--   "ISP1362 VHDL interface for DE2" is free software: you can redistribute it and/or modify
 --    it under the terms of the GNU General Public License as published by
 --    the Free Software Foundation, version 3
 --
---    "ISP1362 Inteface for DE2" is distributed in the hope that it will be useful,
+--    "ISP1362 VHDL interface for DE2" is distributed in the hope that it will be useful,
 --    but WITHOUT ANY WARRANTY; without even the implied warranty of
 --    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 --    GNU General Public License for more details.
 --
 --   You should have received a copy of the GNU General Public License
---   along with "ISP1362 Inteface for DE2".  If not, see <http://www.gnu.org/licenses/>.
+--   along with "ISP1362 VHDL interface for DE2".  If not, see <http://www.gnu.org/licenses/>.
 -- -----------------------------------------------------------------------
 --   ISP1362 register definitions and bit field constants
 -- -----------------------------------------------------------------------
